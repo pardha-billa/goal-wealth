65807718dae198b5033a111f7ae3aeb262c7e7f7
Pramod <pramod@digitalthrottle.com>
2026-08-31 22:42:28 +0530

    Mobile views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_request_variant
   before_action :authenticate_user!
   before_action :load_portfolio_links, if: :user_signed_in?
   helper_method :current_user, :user_signed_in?
 
   private
+
+  def set_request_variant
+    request.variant = :phone if mobile_user_agent?
+  end
+
+  def mobile_user_agent?
+    request.user_agent.to_s.match?(/iPhone|iPod|Android.*Mobile|Windows Phone/i)
+  end
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
